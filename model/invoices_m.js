53cd089f4f5533cfa4cae18358a84324c06c743c
sbxinth <son.son.2013@hotmail.co.th>
2022-09-24 17:00:05 +0700

    fix invoice model

--- model/invoices_m.js
+++ model/invoices_m.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const invoices = new mongoose.Schema({
   _id: mongoose.Types.ObjectId,
   invoices_buyerName: String,
-  invoices_itemList: [{ type: Object, ref: "Products", required: true },{Invoice_Amount:Number}],
+  invoices_itemList: [{product:{
+    _id : mongoose.Types.ObjectId,
+    Product_name: String,
+    Product_detail: [{ Product_detail_color : String, Product_detail_weight : Number}],
+    Product_price: Number}},{Invoice_Amount:Number}],
   invoices_totalPrice : Number
 });
 
